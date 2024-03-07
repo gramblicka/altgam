b6eece057a2741b9ffa49d5562a1fdb460885377
gramblicka <daniel.gramblicka@ringier.sk>
2024-03-07 22:43:58 +0100

    init demo

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import {OneTimePassword} from '@altgram/vue3-one-time-password'
+import "@altgram/vue3-one-time-password/style.css"
+</script>
+
+<template>
+	<div>
+
+		<h4>Divided</h4>
+		<OneTimePassword/>
+
+		<h4>Group</h4>
+		<OneTimePassword otp-mode="group">
+			<p>Error message</p>
+		</OneTimePassword>
+
+
+		<!--		customActiveInputClass,-->
+		<!--		customInputClass,-->
+		<!--		customWrapperClass,-->
+		<!--		reverseDirection-->
+		<!--		borderRadius-->
+		<!--		inputCount-->
+		<!--		otpMode-->
+		<!--		placeholder-->
+		<!--		inputsGapWidth-->
+		<!--		isDisabled-->
+		<!--		isValid-->
+		<!--		isAutoFocus-->
+		<!--		inputType-->
+		<!--		customWrapperValidClass,-->
+		<!--		customWrapperErrorClass,-->
+	</div>
+</template>
+
+<style scoped>
+
+</style>
